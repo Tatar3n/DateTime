574066df4375e9d122111c20713a4fdcec9a9e86
Иван Чуб <ivanchub2104@gmail.com>
2022-10-06 14:38:25 +0300

    Add function cntSecInMin in DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,4 +1,7 @@
 ﻿begin
   var x := DateTime.Now.Year;
   println('Високосный год:',((x mod 4 = 0) and (x mod 100 <> 0) or (x mod 400 = 0))); 
+  
+  var min := 5; // кол-во минут
+  println('Количество секунд в минуте:', min*60);
 end.
