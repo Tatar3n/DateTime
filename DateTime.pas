d1e1eee935aeccb6d71be165c3b8990c37c571c3
Иван Чуб <ivanchub2104@gmail.com>
2022-10-06 14:49:33 +0300

    Add function WhoIsCloser in DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,4 +1,7 @@
 ﻿begin
   var x := DateTime.Now.Year;
   println('Високосный год:',((x mod 4 = 0) and (x mod 100 <> 0) or (x mod 400 = 0))); 
+  
+  var (a,b) := readInteger2();
+  println('день а ближе чем день b:', a > b);
 end.
