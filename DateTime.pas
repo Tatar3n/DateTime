81308d523209711f6badbd5195b6435b0ea96022
Иван Чуб <ivanchub2104@gmail.com>
2022-10-06 14:52:56 +0300

    add function cntDays in DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -4,4 +4,9 @@
   
   var (a,b) := readInteger2();
   println('день а ближе чем день b:', a > b);
+  
+  if (((x mod 4 = 0) and (x mod 100 <> 0) or (x mod 400 = 0))) then
+    println(366)
+  else
+    println(365);
 end.
